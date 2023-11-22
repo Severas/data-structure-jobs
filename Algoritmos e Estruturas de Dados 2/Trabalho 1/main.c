41c21bf6fd26480426a16f7cc4afc78ae34a25cc
Severas <82472575+Severas@users.noreply.github.com>
2023-11-21 21:50:17 -0300

    update

--- Algoritmos e Estruturas de Dados 2/Trabalho 1/main.c
+++ Algoritmos e Estruturas de Dados 2/Trabalho 1/main.c
@@ -17,6 +17,7 @@ enum TrieStatus { LIVRE, OCUPADO };
 typedef struct Trie
 {
     struct Trie *children[CHAR_MAX];
+    char data;
     TrieStatus status;
     int brandStruct;
 }Trie;
@@ -28,13 +29,12 @@ typedef struct Trie
 
 
 Trie* searchTrieRecursive(Trie* T, unsigned char *key, int sizeKey, int lenghtTrie){
-    printf("estou aqui\n");
     if (T == NULL)
-        return 111;
+        return 99999; //inicialmente utilizei NULL como retorno, porem ao fazer nova pesquisa apos remover o id retornava NULL atribuia 0 (zero), dado essa peculiaridade atribui o  valor 99999 como "99999 Trie nao existente." 
     if(sizeKey == lenghtTrie){
-        printf("%d", T->brandStruct);
         return T->brandStruct;
     }
+
     return searchTrieRecursive(T->children[key[lenghtTrie]], key, sizeKey,lenghtTrie+1);
 }
 
@@ -43,6 +43,7 @@ Trie* createNoh(){
     Trie* noh;
     noh = malloc(sizeof(Trie));
     noh->status = LIVRE;
+    //noh->data=data;
     for ( i = 0; i < CHAR_MAX; i++)
     {
         noh->children[i] = NULL;
@@ -51,13 +52,20 @@ Trie* createNoh(){
 }
 
 void insertTrieRecursive(Trie **T, unsigned char *key, int brandStruct, int sizeKey, int lenghtTrie){
-    if(*T == NULL)
-        *T =  createNoh();
+    if(*T == NULL){
+        *T =  createNoh(key[lenghtTrie]);
+        (*T)->brandStruct = 99999;// valor para atribuir a noh sem dono, para facilitar o menu
+    }
     if(sizeKey == lenghtTrie){// inserie o valor e marcar o ultimo caracter
         (*T)->brandStruct = brandStruct;
         (*T)->status = OCUPADO;
+        //(*T)->data = key[lenghtTrie];
+        //printf("%c - ", key[lenghtTrie]);
         return;
     }
+    (*T)->data = key[lenghtTrie];
+    
+    //printf("%c - ",(*T)->data);
     return insertTrieRecursive(&(*T)->children[key[lenghtTrie]],key,brandStruct,sizeKey,lenghtTrie+1);
 }
 
@@ -79,7 +87,7 @@ void removeKeyRecursive(Trie **T, unsigned char *key, int sizeKey, int lenghtTri
         if((*T)->children[i]!=NULL)
             return;
     }
-
+    ///(*T)->brandStruct=NULL;
     free(*T); //liberar
     *T = NULL; // apagar para o  pai nao apontar para ele
 }
@@ -100,18 +108,65 @@ void insertTrie(Trie **T, unsigned char *key, int brandStruct){
     return insertTrieRecursive(T, key, brandStruct, strlen(key), 0);
 }
 
-Trie* searchTrie(Trie* T, unsigned char *key){
+Trie* searchTrie(Trie *T, unsigned char *key){
     return searchTrieRecursive(T, key, strlen(key), 0);
+}
+/*Separa a Sring, insere os elementos na struct e o ID na arvore.*/
+void setNameAge(Trie *T, char *tempLine, dataUser *data,int *i, int *j,int *lines){
+    char sup2[MAX_LINE_SIZE], sup1[2], *token;
+    int assistant;
+                strcpy(sup2,tempLine);//copia a string para facilitar a manipulacao
+                assistant=strcspn(tempLine,"0123456789");// extrai ponto onde comecao os numeros
+                
+                sup1[0]=sup2[assistant];
+                if(sup2[assistant+1]=='\n') // caso a idade tnh apenas um digito
+                    sup1[1]=0;//corta caracteres desnecessarios
+                else
+                    sup1[1]=sup2[assistant+1];
+                sup1[2]=0;//corta caracteres desnecessarios
+                //sup1 recebe os digitos referente a idade do usuario
+                tempLine[assistant]='\0';///atribui \0 onde comeca os numeros
+    
+                strcpy(data[*i].age,sup1);//copia a idade do usuario para a struct
+                token=strtok(tempLine," ");//divide a string em espacos em  branco
+                while (token!=NULL)
+                {
+                    if(*j==0)
+                        strcpy(data[*i].id, token), *j+=1;
+                    else
+                        strcat(data[*i].name, token),
+                        strcat(data[*i].name, " ");
+                        //concatena os nomes para dentro da struct.
+                    token=strtok(NULL, " ");
+                }
+                if(*i>0 && *i<=lines)
+                    insertTrie(T, &(data[*i].id[0]), *i);
+                *j=0;
+                *i+=1;
+            
+    
+}
+
+void textProcessing(char *menu, char *tokenn){
+    int x;
+        for (x = 4; ; x++) {
+            if (menu[x] == ' ' || menu[x] == '\n')
+                break;
+            tokenn[x-4] = menu[x];
+        }
+        tokenn[x-4] = '\0';
+}
+void textProcessingTwo(char *menu){
+    menu[0]=' ';
+    menu[1]=' ';
 }
 
 int main()
 {
-    FILE* arq;
-    char cod,tempLine[MAX_LINE_SIZE], *token,menu[MAX_ID_SIZE+1]="Come abacate, bem. faz bem!";
-    int linhas=0,i=0,j=0,linhasSomando=0,seila;
-    char sup2[MAX_LINE_SIZE],sup1[MAX_AGE_SIZE];
+    FILE *arq;
+    char cod,tempLine[MAX_LINE_SIZE], *token, tokenn[1000],menu[MAX_ID_SIZE+1]="Come abacate, bem. faz bem!";
+    int lines=0,i=0,j=0,linhasSomando=0,assistant;
     Trie *T = NULL;
-    Trie *Tsup = NULL;
     // open the arquivo.txt
     arq= fopen("banco.txt","r+");
     //verfica se  existe erro na leitura
@@ -121,66 +176,62 @@ int main()
     }
     //faz a leitura do primeiro elemento do txt, o qual eh por informar o numero pix cadastrados
     cod = fgetc(arq);
-    // faz conversao de char para int
-    linhas=cod-'0';
-    linhasSomando+=linhas;
-    dataUser *data=(dataUser*)malloc((linhas+1)*sizeof(dataUser));
+    // faz conversao de  char para int
+    lines=cod-'0';
+    linhasSomando+=lines;
+    dataUser *data = (dataUser*) malloc((lines+1)*sizeof(dataUser));
     
     if(arq == NULL)
         printf("Erro, nao foi possivel abrir o arquivo\n");
     else
         while( (fgets(tempLine, sizeof(tempLine), arq))!=NULL )
-        {
-            strcpy(sup2,tempLine);//copia a string para facilitar a manipulacao
-            seila=strcspn(tempLine,"0123456789");// extrai posicao onde comecao os numeros
-            //printf("%s\n e o seila %d", sup2,seila);
-            sup1[0]=sup2[seila];
-            if(sup2[seila+1]=='\n') // caso a idade tnh apenas um digito
-                sup1[1]=0;
-            else
-                sup1[1]=sup2[seila+1];
-            //sup1 recebe os digitos referente a idade do usuario
-            tempLine[seila]='\0';///atribui \0 onde comeca os numeros
-            strcpy(data[i].age,sup1);//copia a idade do usuario para a struct
-            token=strtok(tempLine," ");//divide a string em espacos em  branco
-            
-            while (token!=NULL)
-            {
-                if(j==0)
-                    strcpy(data[i].id, token), j+=1;
-                else
-                    strcat(data[i].name, token),
-                    strcat(data[i].name, " ");
-                    //concatena os nomes para dentro da struct.
-                token=strtok(NULL, " ");
-            }
-            printf("%d o i %s o id\n", i, data[i].age);
-            if(i>0 && i<linhas){
-                insertTrie(&T, &(data[i].id[0]), i);
-            }
-            j=0;
-            i+=1;
-        }
+            printf("%s\n", tempLine),
+            setNameAge(&T,&tempLine,data,&i,&j,&lines); //faz o tratamento da string e armazena na struct data.
 
-        //testar remove 
-        seila=searchTrie(T, "dafd");
-                printf("\nmarcacao %d\n", seila);
         // MENU
         while (menu[0]!='F')
         {
             fgets(menu,MAX_LINE_SIZE,stdin);
-            switch(menu[0]){
+            switch(menu[2]){
                 case '?':
+                    textProcessing(menu,tokenn);//usar para remover \n
+                    assistant=searchTrie(T, tokenn);///retorna posicao de onde estara dentro da struct.
+                    if(assistant==99999)
+                        printf("ID %s nao encontrado.\n",tokenn);
+                    else
+                        data[assistant].name[(strlen(data[assistant].name)-1)]='\0',
+                        printf("(%s|%s|%s)\n", data[assistant].id,data[assistant].name,data[assistant].age);
                     break;
                 case '+':
+                    lines+=1;
+                    menu[2]=' ';
+                    data=(dataUser*) realloc(data,(lines+1)*sizeof(dataUser));
+                    textProcessingTwo(menu);//usar para remover > +
+                    setNameAge(&T,&menu,data,&i,&j,&lines);
+                    textProcessing(menu,tokenn);
+                    
+                    assistant=searchTrie(T, tokenn);
+                    data[assistant].name[(strlen(data[assistant].name)-1)]='\0',
+                    printf("Inserido (%s|%s|%s)\n", data[assistant].id,data[assistant].name,data[assistant].age);
                     break;
                 case '-':
+                // remover da arvore
+
+                    
+                    //utilizar funcao de busca para retornar onde esta a struct
+                    removeKey(&T, token[1]);
+                    
                     break;
                 case 'P':
+
+                    
                     break;
                 case 'S':
+                //salvar alteracao em um novo txt
+                //lines vai precisar de lines+1 o numero de linhas nao esta salvo
                     break;
                 case 'F':
+                //finalizar a execucao do programa;
                     break;
                 default:
                     printf("Desculpe, nao entendi sua resposta");
@@ -190,6 +241,7 @@ int main()
     
     fclose(arq);// fechar arquivo
     free(data);//liberar memoria
+    free(tokenn);
     return 0;
     
     
